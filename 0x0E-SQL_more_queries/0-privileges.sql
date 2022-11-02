8508143d74ff676aecfbe0ddc5b988fc9b2f4689
Brandon lee <leevash203@gmail.com>
2022-11-02 14:12:17 -0700

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on server.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(id int UNIQUE PRIMARY KEY AUTO_INCREMENT, name varchar(256) NOT NULL);
+
+-- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on server(localhost).
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
